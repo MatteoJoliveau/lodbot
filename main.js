a85e9fa471640693f8c60d14e01ab95f51be930d
Matteo Joliveau <m.joliveau@koor.it>
2017-06-19 10:37:39 +0200

    main

--- main.js
+++ main.js
@@ -8,8 +8,11 @@ const config = utils.loadConfig(path.resolve(__dirname + "/config.yml"));
 
 const bot = new Telegraf(config.telegram.token);
 
-bot.command("test", (ctx) => {
-  console.log("Hello!");
+bot.command("start", (ctx) => {
+  const reply = "<b>HOWDY FELLOW LOOKER!</b>\nWelcome to <b>Look of Disapproval Bot</b>, a friendly bot to friendly spam friendly faces :D.\nIf a face does not"
+  + "appear in the selection, feel free to add it yourself! Follow our <a href="">contribution guide</a> to learn how to do it.\n"
+  + "<b>HAPPY SPAM!</b>"
+  ctx.replyWithHTML(reply);
 })
 
 bot.on('inline_query', (ctx) => {
