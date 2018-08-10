70de4bcb46d739d72abf7cefc3527f572b581a04
Matteo Joliveau <matteo.joliveau@mikamai.com>
2018-08-10 21:15:10 +0200

    Fixes

--- main.js
+++ main.js
@@ -11,21 +11,21 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 
 bot.command("start", (ctx) => {
-  const reply = "<b>HOWDY FELLOW LOOKER!</b>\nWelcome to <b>Look of Disapproval Bot</b>, a friendly bot to friendly spam friendly faces :D.\nIf a face does not" +
-    "appear in the selection, feel free to add it yourself! Follow our <a href=\"https://github.com/MatteoJoliveau/lodbot/blob/master/CONTRIBUTING.md\">contribution guide</a> to learn how to do it.\n" +
-    "<b>HAPPY SPAM!</b>"
+  const reply = `
+<b>HOWDY FELLOW LOOKER!</b>
+Welcome to <b>Look of Disapproval Bot</b>, a friendly bot to friendly spam friendly faces :D.
+If a face does not appear in the selection, feel free to add it yourself! Follow our <a href="https://github.com/MatteoJoliveau/lodbot/blob/master/CONTRIBUTING.md">contribution guide</a> to learn how to do it.
+<b>HAPPY SPAM!</b>
+`
   ctx.replyWithHTML(reply);
 })
 
-bot.on('inline_query', (ctx) => {
-  if (ctx.inlineQuery.query === "") {
-    ctx.answerInlineQuery(all_faces, { cache_time: config.telegram.inline_cache });
+bot.on('inline_query', ({ inlineQuery: { query }, answerInlineQuery }) => {
+  if (query === '') {
+    answerInlineQuery(all_faces, { cache_time: config.telegram.inline_cache });
   } else {
-    const faces = all_faces.filter((item) => {
-      const string = ctx.inlineQuery.query.toLowerCase();
-      return item.title.toLowerCase().search(string) != -1;
-    });
-    ctx.answerInlineQuery(faces, { cache_time: config.telegram.inline_cache })
+    const faces = all_faces.filter((item) => (item.title.toLowerCase().search(query.toLowerCase()) !== -1));
+    answerInlineQuery(faces, { cache_time: config.telegram.inline_cache })
   }
 
 });
